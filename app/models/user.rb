bc14f7cf19793264856468b052bb6639f323aa6e
Júlia von der Heyde <juliavdheyde@icloud.com>
2023-06-06 12:27:18 -0300

    doing devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   belongs_to :route
   has_many :favorites, :rides, :routes
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_05_185518) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_06_144528) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_05_185518) do
     t.datetime "updated_at", null: false
     t.index ["route_id"], name: "index_favorites_on_route_id"
     t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "models", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_models_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
   end
 
   create_table "rides", force: :cascade do |t|
