772827de5bc5a94e8d438772e1799525c877c8ef
Júlia von der Heyde <juliavdheyde@icloud.com>
2023-06-06 15:32:59 -0300

    created devise

--- db/schema.rb
+++ db/schema.rb
@@ -23,18 +23,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_170326) do
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
-  create_table "models", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_models_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
-  end
-
   create_table "points", force: :cascade do |t|
     t.bigint "route_id", null: false
     t.datetime "created_at", null: false
@@ -65,12 +53,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_170326) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "user_name"
-    t.string "email"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "pinned_route_id"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["pinned_route_id"], name: "index_users_on_pinned_route_id"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "favorites", "routes"
