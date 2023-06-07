5004f61b0ded027fe935b3c1bc9bec248f4ff9d8
Alphonse Bonnin <alphonse.bonnin@gmail.com>
2023-06-07 14:39:48 -0300

    pundit adjustment

--- app/policies/route_policy.rb
+++ app/policies/route_policy.rb
@@ -1,28 +1,28 @@
 class RoutePolicy < ApplicationPolicy
   class Scope < Scope
 
-      def resolve
-        scope.all
-      end
+    def resolve
+      scope.all
+    end
 
-      def show?
-        true
-      end
+    def show?
+      true
+    end
 
-      def create?
-        true
-      end
+    def create?
+      true
+    end
 
-      def update?
-        record.user == user
-      end
+    def update?
+      record.creator == user
+    end
 
-      def edit?
-        update?
-      end
+    def edit?
+      update?
+    end
 
-      def destroy?
-        record.user == user
-      end
+    def destroy?
+      record.user == user
+    end
   end
 end
