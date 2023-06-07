265685a8e00e7eed77f385859a473fc8ede2654c
Júlia von der Heyde <juliavdheyde@icloud.com>
2023-06-07 14:45:43 -0300

    improved seed

--- app/models/route.rb
+++ app/models/route.rb
@@ -2,4 +2,5 @@ class Route < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :rides
   has_many :favorites, dependent: :destroy
+  has_many :points, dependent: :destroy
 end
--- db/migrate/20230606213046_add_latitude_and_longitude_to_points.rb
+++
@@ -1,6 +0,0 @@
-class AddLatitudeAndLongitudeToPoints < ActiveRecord::Migration[7.0]
-  def change
-    add_column :points, :latitude, :float
-    add_column :points, :longitude, :float
-  end
-end
