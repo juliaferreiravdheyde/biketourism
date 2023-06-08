71b2e615302c700eda0c631b7a01f02072d9fb47
thiago laredo <thiago@olatu.com.br>
2023-06-08 19:22:49 -0300

    render dos códigos e card por type

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
     @routes = Route.all
+    # @best_routes = Route.order(votes: :desc).first(5)
   end
 end
