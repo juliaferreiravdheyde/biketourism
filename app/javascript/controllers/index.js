5b6bec5815a3f01a5a37c84638d2aefa95df6799
Júlia von der Heyde <juliavdheyde@icloud.com>
2023-06-15 11:19:16 -0300

    stimulus controller done

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import FavoritesController from "./favorites_controller"
+application.register("favorites", FavoritesController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
