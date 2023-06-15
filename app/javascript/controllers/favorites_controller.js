5a3dbd6f71fb3e71656ce1f7d60aa9138fd17f90
Júlia von der Heyde <juliavdheyde@icloud.com>
2023-06-15 09:53:35 -0300

    done stimulus to favorites

---
+++ app/javascript/controllers/favorites_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="favorites"
+export default class extends Controller {
+  static targets = ["heart"];
+  connect() {
+  }
+
+  toggleFavorite(event) {
+    event.preventDefault();
+    const heart = this.heartTarget;
+    const url = this.element.getAttribute("href");
+
+    fetch(url, { method: "POST", headers: { "X-Requested-With": "XMLHttpRequest" } })
+      .then((response) => response.text())
+      .then((html) => {
+        heart.innerHTML = html;
+      });
+  }
+}
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,12 +4,17 @@
 
 import { application } from "./application"
 
+import FavoritesController from "./favorites_controller"
+application.register("favorites", FavoritesController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
 import MapController from "./map_controller"
 application.register("map", MapController)
 
+import PinnedController from "./pinned_controller"
+application.register("pinned", PinnedController)
+
 import RouteTrackingController from "./route_tracking_controller"
 application.register("route-tracking", RouteTrackingController)
-
